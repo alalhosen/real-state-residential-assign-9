3cd4dcc3fb27433728b9abef8080b6e373e01d94
Alal Hosen <alalhosen1992620120@gmail.com>
2025-12-19 23:02:39 +0600

    navber three ancor tag set

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,11 @@ const Header = () => {
   return (
     <div>
       <h1>This is header.</h1>
+      <div>
+        <a href="/">Home</a>
+        <a href="updateProfile">Update Profile</a>
+        <a href="userProfile">User Profile</a>
+      </div>
     </div>
   );
 };
